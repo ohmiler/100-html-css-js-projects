eed14f3eb6e457340080a1e54bbbcb8f3fb5d8d1
miler <misterpatipan123@gmail.com>
2025-02-19 23:55:18 +0700

    Add files via upload

---
+++ 04-toggle-search-bar/assets/js/script.js
@@ -0,0 +1,8 @@
+let searchIcon = document.getElementById('searchIcon');
+let myForm = document.getElementById('myForm');
+
+searchIcon.addEventListener('click', () => {
+    myForm.classList.toggle('show');
+})
+
+
