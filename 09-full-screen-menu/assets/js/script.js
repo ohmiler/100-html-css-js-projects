976bdcc916e64520229e1b16fd20f11980125cf1
miler <misterpatipan123@gmail.com>
2025-06-25 03:41:41 +0700

    Add files via upload

--- 09-full-screen-menu/assets/js/script.js
+++ 09-full-screen-menu/assets/js/script.js
@@ -2,23 +2,21 @@ const menuToggle = document.querySelector('.menu-toggle');
 const overlay = document.querySelector('.overlay');
 
 menuToggle.addEventListener('click', function() {
-  menuToggle.classList.toggle('open');
-  overlay.classList.toggle('open');
-  
-  // Prevent scrolling when menu is open
-  if (overlay.classList.contains('open')) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = 'auto';
-  }
+    overlay.classList.toggle('open');
+
+    // Prevent scrolling when menu is open
+    if (overlay.classList.contains('open')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'auto';
+    }
 });
 
-// Close menu when clicking a menu item
+// Close menu when click a menu items
 const menuItems = document.querySelectorAll('.menu a');
 menuItems.forEach(item => {
-  item.addEventListener('click', function() {
-    menuToggle.classList.remove('open');
-    overlay.classList.remove('open');
-    document.body.style.overflow = 'auto';
-  });
-});
+    item.addEventListener('click', function() {
+        overlay.classList.remove('open');
+        document.body.style.overflow = 'auto';
+    })
+})
